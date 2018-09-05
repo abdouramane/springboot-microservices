4d51477820004facc4dfb731256bb4659b3d40fd
abdouramane <abdouramanesoumaila@gmail.com>
2018-09-05 11:37:02 +0200

    add new service /count to counterservice application

--- counterservice/src/test/java/com/doura/microservices/counterservice/CounterServiceApplicationTests.java
+++ counterservice/src/test/java/com/doura/microservices/counterservice/CounterServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CounterserviceApplicationTests {
+public class CounterServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
